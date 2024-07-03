e9e46f5daca6aa768b94de6e9fedb13bb9a07c13
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-07-03 21:23:29 +0600

    Footer running

--- src/Component/MainLayout/MainLayout.tsx
+++ src/Component/MainLayout/MainLayout.tsx
@@ -4,18 +4,24 @@ import FrequentlyAsk from "../AllSection/FrequentlyAsk/FrequentlyAsk";
 import HelpSolution from "../AllSection/HelpSolution/HelpSolution";
 import HeroSection from "../AllSection/HeroSection/HeroSection";
 import Testimonial from "../AllSection/Testimonial/Testimonial";
+import Footer from "../Footer/Footer";
 import NavBar from "../NavBar/NavBar";
 
 const MainLayout = () => {
   return (
-    <div className="max-w-[1160px] mx-auto px-5 pb-10">
-      <NavBar />
-      <HeroSection />
-      <HelpSolution />
-      <EmpoweringHealth />
-      <Testimonial />
-      <FrequentlyAsk />
-      <FirstAppointment />
+    <div>
+      <div className="max-w-[1160px] mx-auto px-5">
+        <NavBar />
+        <HeroSection />
+        <HelpSolution />
+        <EmpoweringHealth />
+        <Testimonial />
+        <FrequentlyAsk />
+        <FirstAppointment />
+      </div>
+      <div className="bg-[#020043]">
+        <Footer />
+      </div>
     </div>
   );
 };
